904b0176734daea59fed4fdcb61ca8ad17ba3a31
TECHRITOMA Inc <ajuatah@gmail.com>
2022-06-30 04:59:54 +0100

    More features added

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeQnswer { get; set; }
+
+        //ctor generate the constructor automatically pressing tab twice
+        public Joke()
+        {
+
+        }
+    }
+}
